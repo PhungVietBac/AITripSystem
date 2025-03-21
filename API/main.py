03a4aa82f78292125f9a3fd3234b618ab80b325b
VanQuocNguyen <22521215@gm.uit.edu.vn>
2025-03-21 22:04:31 +0700

    Update friends, update Booking API

--- API/main.py
+++ API/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from controllers import friend_ctrl
+from controllers import friend_ctrl, booking_ctrl
 
 app = FastAPI()
 
 app.include_router(friend_ctrl.router, prefix="/api/v1", tags=["friends"])
+app.include_router(booking_ctrl.router, prefix="/api/v1", tags=["bookings"])
--- API/repositories/friend_repo.py
+++ API/repositories/friend_repo.py
@@ -3,16 +3,9 @@ from models.friend import Friend
 from schemas.friend_schema import FriendCreate
 
 def get_friends_by_user(db: Session, user_id: str):
-    """
-    Lấy danh sách bạn bè của một người dùng dựa trên user_id.
-    Trả về tất cả các quan hệ bạn bè mà user_id là IDSelf hoặc IDFriend.
-    """
     return db.query(Friend).filter((Friend.idSelf == user_id) | (Friend.idFriend == user_id)).all()
 
 def create_friend(db: Session, friend: FriendCreate):
-    """
-    Tạo một quan hệ bạn bè mới trong bảng Friends.
-    """
     # Tạo đối tượng Friend từ dữ liệu đầu vào
     db_friend = Friend(idSelf=friend.idSelf, idFriend=friend.idFriend, isAccept=friend.isAccept)
     db.add(db_friend)
@@ -21,10 +14,6 @@ def create_friend(db: Session, friend: FriendCreate):
     return db_friend
 
 def delete_friend(db: Session, id_self: str, id_friend: str):
-    """
-    Xóa một quan hệ bạn bè dựa trên id_self và id_friend.
-    Trả về True nếu xóa thành công, False nếu không tìm thấy.
-    """
     friend = db.query(Friend).filter(Friend.idSelf == id_self, Friend.idFriend == id_friend).first()
     if not friend:
         return False
