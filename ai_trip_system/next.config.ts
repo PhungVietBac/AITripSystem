792c393bd72dae5545dd10cfdc07dd710cf74639
VanQuocNguyen <22521215@gm.uit.edu.vn>
2025-04-15 18:37:26 +0700

    Update Map Page, fix Header

--- ai_trip_system/next.config.ts
+++ ai_trip_system/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['bcp.cdnchinhphu.vn'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bcp.cdnchinhphu.vn',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
--- ai_trip_system/src/components/footer.tsx
+++ ai_trip_system/src/components/footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>footer</div>;
+  return <div className="flex bg-gray-950 text-white h-32 justify-center items-center">
+    <h1 className="text-3xl">
+      This is a footer
+    </h1>
+  </div>;
 };
 
 export default Footer;
