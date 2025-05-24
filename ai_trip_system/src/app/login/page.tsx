3ab8716b7e20fd97a8ea6266a63151d2056c42f3
tnduya3 <23520394@gm.uit.edu.vn>
2025-05-24 21:52:07 +0700

    fix link API register, update title page

--- ai_trip_system/src/app/login/page.tsx
+++ ai_trip_system/src/app/login/page.tsx
@@ -1,4 +1,11 @@
 import LoginForm from "./loginForm";
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login to your account',
+  description: 'This is the login page for users to access their accounts.',
+};
+
 
 export default function LoginPage() {
     return (
