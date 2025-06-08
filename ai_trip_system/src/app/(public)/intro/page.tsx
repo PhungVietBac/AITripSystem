37d5d519e1997b0a653937c5b76f632aab21ebfd
VietBac <22520089@gm.uit.edu.vn>
2025-06-08 20:26:49 +0700

    Fix intro page

--- ai_trip_system/src/app/(public)/intro/page.tsx
+++ ai_trip_system/src/app/(public)/intro/page.tsx
@@ -1,8 +1,9 @@
 import Feature from "./Feature";
 import HeroSections from "./herosection";
 import About from "./About";
-import Members from "./Members";
+import Members from "./members";
 import Instruction from "./Instruction";
+import Image from "next/image";
 
 export default function IntroPage() {
   return (
@@ -12,18 +13,22 @@ export default function IntroPage() {
       {/* Starfish decorations between Hero and About */}
       <div className="relative h-16">
         <div className="absolute top-2 -left-5 z-10">
-          <img
+          <Image
             src="/images/saobien.png"
             alt="Sao biển"
             className="w-56 h-56 opacity-70 animate-pulse"
+            width={500}
+            height={500}
           />
         </div>
         <div className="absolute -top-24 right-20 z-10">
-          <img
+          <Image
             src="/images/saobien.png"
             alt="Sao biển"
             className="w-36 h-36 opacity-50 animate-bounce"
-            style={{ animationDelay: '1s' }}
+            style={{ animationDelay: "1s" }}
+            width={500}
+            height={500}
           />
         </div>
       </div>
